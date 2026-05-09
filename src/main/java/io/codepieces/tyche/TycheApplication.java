bceb736eac19f7778d2fba6b8516050d52d730d3
Jose Perez <joseperez@Joses-MacBook-Pro.local>
2026-05-09 11:03:23 +0100

    Initial commit

---
+++ src/main/java/io/codepieces/tyche/TycheApplication.java
@@ -0,0 +1,13 @@
+package io.codepieces.tyche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TycheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TycheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/codepieces/tyche/TycheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codepieces.tyche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TycheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
